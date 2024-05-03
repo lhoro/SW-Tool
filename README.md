31926eeceaff3001f13a057f248f3ff49623599b
lhoro <32704441+lhoro@users.noreply.github.com>
2024-05-03 09:31:14 +0200

    POPUP
    - Prosty popup
    -Niezbędne skrypty startowe

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Narzędzie do gry Shinobiworld.
+
+- Uruchomiono rozszeżenie
+    * Utworzono prosty popup i niezbędne skrypty.
---
+++ js/popup.js
@@ -0,0 +1,24 @@
+let button1 = document.getElementById('button1')
+button1.addEventListener("click",  () => {
+    alert("Button 1")
+});
+
+let button2 = document.getElementById('button2')
+button2.addEventListener("click", () => {
+    alert("Button 2")
+});
+
+let button3 = document.getElementById('button3')
+button3.addEventListener("click", () => {
+    alert("Button 3")
+});
+
+let button4 = document.getElementById('button4')
+button4.addEventListener("click", () => {
+    alert("Button 4")
+});
+
+let button5 = document.getElementById('button5')
+button5.addEventListener("click", () => {
+    alert("Button 5")
+});
